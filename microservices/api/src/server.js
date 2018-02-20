4342a44e9cfa54f4aabe9ed228a243f63bc82893
Bhushan A Jangle <31948220+waffwaff1@users.noreply.github.com>
2018-02-21 02:19:40 +0530

    Update server.js

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const {Wit} = require('node-wit');
 const client = new Wit({accessToken: '4HPNRD6HZ5WJFMPTDSIXLUEAXASGQCHV'});
-app.set('public', path.join(__dirname, '../client/public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -18,7 +14,6 @@ app.post('/api', function (req, res) {
   })
   .catch(console.error);
 });
-
 
 app.listen(1000,function(){
     console.log('server started on port 1000...')
