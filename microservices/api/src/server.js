5de834571c710a17d0daf554584623e1d3134cf2
Bhushan A Jangle <31948220+waffwaff1@users.noreply.github.com>
2018-02-21 21:57:34 +0530

    Update server.js

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -1,11 +1,17 @@
+//Upadating Module
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const {Wit} = require('node-wit');
 const client = new Wit({accessToken: '4HPNRD6HZ5WJFMPTDSIXLUEAXASGQCHV'});
+
+// Init App
+const app = express();
+
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//A route for api post method
 app.post('/api', function (req, res) {
   let data = req.body.data;
   resp=client.message(data, {error: null})
@@ -15,6 +21,7 @@ app.post('/api', function (req, res) {
   .catch(console.error);
 });
 
+//Starts the webserver (router) on port 1000 and listens for connections
 app.listen(1000,function(){
     console.log('server started on port 1000...')
 });
